ca899cb0f669cc6e70373e17cd723ac84304d8e8
Artem Tikhomirov <artem.tikhomirov.nn@gmail.com>
2022-12-12 15:22:56 +0300

    Add Example007, 008, 010, 011 Method and Arrau, MehtodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] array = { 12, 92, 34, 42, 885, 36, 67, 81 };
+
+int n = array.Length;
+int find = 42;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index + 1);
+        break;
+    }
+    index++;
+}
